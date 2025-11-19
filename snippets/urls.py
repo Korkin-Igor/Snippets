258827b706e3a1d314c078b4a45593f2ab40e880
Korkin-Igor <korkinigorttit@gmail.com>
2025-11-19 10:52:41 +0700

    fix urls

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-   path('snippets/', views.SnippetList.as_view()),
-   path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+   path('', include(router.urls)),
 ]
